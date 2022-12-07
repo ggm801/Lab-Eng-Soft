ddb64396de46d5c274787b1600a0158c5065e11f
ArthurTheillierUSP <114691933+ArthurTheillierUSP@users.noreply.github.com>
2022-12-07 15:53:14 -0300

    Atualização working

--- MyProject/book/models.py
+++ MyProject/book/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 
-
 class Voo(models.Model):
-    
-
-
     ID = models.IntegerField(primary_key=True)
     DH_PREVISTO_SAIDA = models.DateTimeField(auto_now=False)
     DH_PREVISTO_CHEGADA = models.DateTimeField(auto_now=False)
@@ -20,28 +14,24 @@ class Voo(models.Model):
 
     class Meta:
         db_table = 'Voo'
-    def clean(self) :
-        if self.DH_PREVISTO_SAIDA > self.DH_PREVISTO_CHEGADA :
-            raise ValidationError('Start date cannot precede end date')
-    
-    def __str__(self):
-        return str(self.DH_PREVISTO_SAIDA)
-
-
-    def save(self, *args, **kwargs):
-        self.clean()
-        return super().save(*args, **kwargs)
-
-    
 
 
 class VooReal(models.Model):
+    departure_status_choices = [
+        ('Programado', 'Programado'),
+        ('Embarcando', 'Embarcando'),
+        ('Cancelado', 'Cancelado'),
+        ('Taxeando', 'Taxeando'),
+        ('Pronto', 'Pronto'),
+        ('Autorizado', 'Autorizado'),
+        ('Em voo', 'Em voo'),
+        ('Pousando', 'Pousando'),
+        ('Atterissado', 'Atterissado'),]
     ID = models.IntegerField(primary_key=True)
     ID_VOO = models.ForeignKey(Voo, on_delete=models.CASCADE)
     DH_REAL_SAIDA = models.DateTimeField(null=True, blank=True,auto_now=False)
     DH_REAL_CHEGADA = models.DateTimeField(null=True, blank=True,auto_now=False   )
-    NM_STATUS = models.CharField(max_length=50, null=False)
-
+    NM_STATUS = models.CharField(max_length=50, null=False, choices=departure_status_choices, default='Programado')
     class Meta:
         db_table = 'VooReal'
 
@@ -60,5 +50,5 @@ class Usuario(models.Model):
 class User(models.Model):
   
     class Meta:
-        permissions = (("view_voo_atual", "can view voo atual"),("access_relatorio", "can access relatorio page"),("generate_relatorio ","can generate relatorio"),("access_atualizar","can access to atualizar voo page"),("edit_atual", "can edit oo real"),)
+        permissions = (("access_relatorio", "can access relatorio page"),("generate_relatorio ","can generate relatorio"),("access_atualizar","can access to atualizar voo page"),)
         
